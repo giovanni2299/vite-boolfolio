0d209642f93758bb2260cea20eec5a28f10884c3
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-06-12 16:29:45 +0200

    ADD: milestone 3,4,5

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="col-3 ">
+        <div class="card h-100 mb-4 p-3">
+            <div>
+                <strong class="text-primary">Title:</strong>
+                <br>
+                {{item.title}}
+
+            </div>
+
+            <div >
+                <strong class="text-primary">Slug:</strong>
+                <br>
+                {{item.slug}}
+
+            </div>
+
+            <div >
+                <strong class="text-primary">Git Link:</strong>
+                <br>
+                {{item.github_link}}
+
+            </div>
+
+            <div >
+                <strong class="text-primary">Description:</strong>
+                <br>
+                {{item.description}}
+
+            </div>
+        </div>
+
+    </div>
+
+       
+    
+           
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+
+            }
+        },
+        props:{
+            item:{
+                type : Object
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
